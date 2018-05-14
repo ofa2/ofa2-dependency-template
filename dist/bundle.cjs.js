d9cff12a28a6e23b9ad5c6277444e134b774ab7c
xinshangshangxin <xinshangshangxin@gmail.com>
2018-05-14 17:45:20 +0800

    chore: remove build uglify

--- dist/bundle.cjs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function lift(config) {
-  // eslint-disable-next-line no-console
-  console.log(config);
-}
-
-module.exports = lift;
-//# sourceMappingURL=bundle.cjs.js.map
--- dist/bundle.esm.js
+++
@@ -1,7 +0,0 @@
-function lift(config) {
-  // eslint-disable-next-line no-console
-  console.log(config);
-}
-
-export default lift;
-//# sourceMappingURL=bundle.esm.js.map
